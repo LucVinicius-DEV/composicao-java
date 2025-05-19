208669d6dbd9511da20ceb6f5d23b912839a9644
Lucas <us3.lucas@gmail.com>
2025-05-19 14:59:15 -0300

    Update Department.java

--- worker-contracts/Department.java
+++ worker-contracts/Department.java
@@ -1,26 +1,25 @@
 package entities;
 
-
-public class Department {  
+// Classe que representa um departamento da empresa
+public class Department {
   
-  private String name;
-
-  public Department () {
+  private String name; // Nome do departamento
+  
+  // Construtor padrão
+  public Department() {
   }
-
+  
+  // Construtor com parâmetro
   public Department(String name) {
     this.name = name;
   }
-
-public String getName() {
-	return name;
-  }
-
-public void setName(String name) {
-	this.name = name;
-  }
-
   
-
+  // Métodos getters e setters
+  public String getName() {
+    return name;
+  }
   
+  public void setName(String name) {
+    this.name = name;
+  }
 }
